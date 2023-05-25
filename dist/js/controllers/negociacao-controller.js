7084aceff40c0944c1c881352796bacc2254dcdf
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-05-25 20:35:05 -0300

    feat: evoluindo meu codigo

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,12 +1,27 @@
+import { Negociacao } from "../models/negociacao.js";
 export class NegociacaoController {
     constructor() {
-        this.inputData = document.querySelector('data');
+        this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('valor');
+        this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValor);
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limpaForm();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(date, quantidade, valor);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limpaForm() {
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
+        this.inputData.focus();
     }
 }
