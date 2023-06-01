6e45dc3840d7c5e39dec61d06e097221a7262e2e
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-06-01 16:09:23 -0300

    feat: melhorando o codigo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -3,14 +3,29 @@ export class Negociacao {
     private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
-  ) { }
+  ) {
+    this.validarValores();
+  }
 
+  private validarValores(): void {
+    if (!Number.isFinite(this.quantidade) || this.quantidade <= 0) {
+      throw new Error('A quantidade deve ser um número positivo.');
+    }
+
+    if (!Number.isFinite(this.valor) || this.valor <= 0) {
+      throw new Error('O valor deve ser um número positivo.');
+    }
+  }
 
   get volume(): number {
     return this.quantidade * this.valor;
   }
+
   get data(): Date {
-    const data = new Date(this.data.getTime());
-    return data;
+    return new Date(this._data.getTime());
+  }
+
+  static criar(data: Date, quantidade: number, valor: number): Negociacao {
+    return new Negociacao(data, quantidade, valor);
   }
 }
---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,11 @@
+export class NegociacoesView {
+  template(): string {
+    return `
+    
+    
+    
+    
+    `;
+
+  }
+}
